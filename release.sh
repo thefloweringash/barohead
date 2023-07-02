84a8c2ea97e5554bbc3170308f63e8c622a6efa1
Andrew Childs <lorne@cons.org.nz>
2023-07-02 14:29:42 +0900

    Cleanup old releases

--- release.sh
+++ release.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 cd barohead
 
 trunk build --dist dist-release --release --public-url /barohead
-rsync -r dist-release/ feyhin.cons.org.nz:/var/www/n.gen.nz/html/barohead
+rsync --delete-after -r dist-release/ feyhin.cons.org.nz:/var/www/n.gen.nz/html/barohead
