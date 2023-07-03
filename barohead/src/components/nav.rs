8c7f1a0c55fdb6e8fd5ad641ea1fc59849ad43ad
Andrew Childs <lorne@cons.org.nz>
2023-07-03 11:20:18 +0900

    Fix the navbar
    Make it usable on mobile

--- barohead/src/components/nav.rs
+++ barohead/src/components/nav.rs
@@ -7,23 +7,39 @@ use crate::components::ItemSearch;
 
 #[function_component(Nav)]
 pub fn nav() -> Html {
+    let is_active = use_state(|| false);
+    let toggle_active = {
+        let is_active = is_active.clone();
+        Callback::from(move |_| is_active.set(!*is_active))
+    };
+
+    let aria_expanded = if *is_active { "true" } else { "false" };
     html! {
-        <nav class="navbar" role="navigation" aria-label="main navigation">
+        <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <Link<Route> to={Route::Home} classes="navbar-item">
                     {"BAROHEAD"}
                 </Link<Route>>
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a
+                    role="button"
+                    class={classes!("navbar-burger", is_active.then(|| Some("is-active")))}
+                    onclick={toggle_active}
+                    aria-label="menu"
+                    aria-expanded={aria_expanded}
+                >
                   <span aria-hidden="true"></span>
                   <span aria-hidden="true"></span>
                   <span aria-hidden="true"></span>
                 </a>
             </div>
-            <div class="navbar-menu">
+            <div class={classes!("navbar-menu", is_active.then(|| Some("is-active")))}>
                 <div class="navbar-start">
                     <div class="navbar-item">
                         <ItemSearch />
                     </div>
+                </div>
+                <div class="navbar-end">
+                    <a class="navbar-item" href="https://github.com/thefloweringash/barohead">{"GitHub"}</a>
                 </div>
             </div>
         </nav>
